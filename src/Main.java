09085551b3995480da456fdb5b0fdf9df83e256f
Anastasiia Rynkevych <nestalirunkevich@gmail.com>
2024-10-11 19:42:04 +0300

    feat: added entry point

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,43 @@
 public class Main {
     public static void main(String[] args) {
-        Matrix matrix = new Matrix(4, 4);
+        Matrix matrix = new Matrix(3, 3);
+
+        // Matrix class
         matrix.fill();
-//        Matrix matrix1 = new Matrix(matrix);
-//        matrix.toLowerTriangular();
-//        matrix1.toUpperTriangular();
+        Matrix matrix1 = new Matrix(matrix);
+        Matrix matrix2 = new Matrix(matrix);
+        Matrix matrix3 = new Matrix(matrix);
+        Matrix matrix4 = new Matrix(matrix);
+        matrix.getPart();
+        matrix.getSize();
+        matrix1.add(matrix2);
+        matrix2.multiplyByScalar(2);
+        matrix1.multiply(matrix2);
+        matrix1.transpose();
+        Matrix.fromVector(new double[] {3, 1, 7});
+        Matrix.identity(3);
+        Matrix.createRowMatrix(3);
+        Matrix.createColumnMatrix(3);
+        matrix3.toLowerTriangular();
+        matrix4.toUpperTriangular();
         matrix.invert();
+
+//        // Immutable class
+//
+//        Immutable immutable = new Immutable(3, 3);
+//        immutable.fill();
+//        Immutable immutable1 = new Immutable(immutable);
+//        immutable.add(immutable1);
+//        immutable.multiplyByScalar(2);
+//        immutable.multiply(immutable1);
+//        immutable.toLowerTriangular();
+//        immutable.toUpperTriangular();
+//
+//        // Generic Matrix class
+//
+//        GenericMatrix<String> genericMatrix = new GenericMatrix<>(3, 3);
+//        genericMatrix.fillFromConsole(String.class);
+//        genericMatrix.printMatrix();
+//        genericMatrix.printElement(1, 2);
     }
 }
